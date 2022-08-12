8ef0a50ca2e1bfbdc55d9bfd19e42e120f268167
Pavel Shkunov <pvlshkunov@gmail.com>
2022-08-12 23:46:46 +0300

    functions work with indexes instead of 2d lists
    Signed-off-by: Pavel Shkunov <pvlshkunov@gmail.com>

--- game.py
+++ game.py
@@ -18,62 +18,38 @@ def to_str(matrix: '2d list') -> str:
     return '\n'.join([' '.join(i) for i in matrix]) + '\n'
 
 
-def get_indexes(in_matrix: '2d list') -> 'list of tuples':
-    indexes = []
-    for i in range(len(in_matrix)):
-        for j in range(len(in_matrix[i])):
-            if in_matrix[i][j] == '0':
-                indexes.append((i, j))
-    return indexes
-
-
-def move(in_matrix: '2d list', direction='down') -> '2d list':
-    n = len(in_matrix)
-    m = len(in_matrix[0])
-    matrix = empty_playground((m, n))
-
-    old = get_indexes(in_matrix)
+def move(old_ind: 'list of tuples', direction='down', dimensions=(10, 10)) -> 'list of tuples':
+    m, n = dimensions
+    border = False
     new = []
-    for i in old:
+    for i in old_ind:
         if direction == 'down':
             new.append((i[0] + 1, i[1]))
-        elif direction == 'right':
+            if i[0] + 1 == n:
+                border = True
+        if direction == 'right':
             new.append((i[0], i[1] + 1))
-        elif direction == 'left':
+            if i[1] + 1 == m:
+                border = True
+        if direction == 'left':
             new.append((i[0], i[1] - 1))
-
-    for i in new:
-        try:
-            matrix[i[0]][i[1]] = '0'
-            if i[1] < 0:
-                raise IndexError
-        except IndexError:
-            return in_matrix
-    return matrix
+            if i[1] - 1 < 0:
+                border = True
+    if border:
+        return old_ind
+    return new
 
 
-def on_floor(in_matrix: '2d list'):
-    return bool([i for i in in_matrix[-1] if i == '0'])
+def on_floor(indexes, dimensions=(10, 10)):
+    return max([i[0] for i in indexes]) == dimensions[1]
 
 
-def on_border(indexes: 'list of tuples', in_matrix: '2d list'):
-    border = [(i[0] - 1, i[1]) for i in indexes]
-    for i in range(len(in_matrix)):
-        for j in range(len(in_matrix[0])):
-            if in_matrix[i][j] == '0' and (i, j) in border:
-                return True
+def on_border(occupied: 'list of tuples', piece: 'list of tuples'):
+    border = [(i[0] - 1, i[1]) for i in occupied]
+    return bool([i for i in piece if i in border])
 
 
-def merge(matrix_1: '2d list', matrix_2: '2d list') -> '2d list':
-    matrix = [i.copy() for i in matrix_1]
-    for i in range(len(matrix_2)):
-        for j in range(len(matrix_2[0])):
-            if matrix_2[i][j] == '0':
-                matrix[i][j] = '0'
-    return matrix
-
-
-def disappear(old_ocupp: 'list of tuples', dimensions=(10, 5)) -> 'list of tuples':
+def disappear(old_ocupp: 'list of tuples', dimensions=(10, 10)) -> 'list of tuples':
     m, _ = dimensions
     new_occup = []
     if old_ocupp:
@@ -97,8 +73,8 @@ def finish(indexes: 'list of tuples'):
             return True
 
 
-def from_ind(indexes: 'list of tuples', dim=(10, 10)):
-    matrix = empty_playground(dim)
+def from_ind(indexes: 'list of tuples', dimensions=(10, 10)):
+    matrix = empty_playground(dimensions)
     for coord in indexes:
         matrix[coord[0]][coord[1]] = '0'
     return matrix
@@ -110,9 +86,12 @@ def main():
     I = [[(0, 4), (1, 4), (2, 4), (3, 4)], [(0, 3), (0, 4), (0, 5), (0, 6)]]
     S = [[(0, 4), (0, 5), (1, 3), (1, 4)], [(0, 4), (1, 4), (1, 5), (2, 5)]]
     Z = [[(0, 4), (0, 5), (1, 5), (1, 6)], [(0, 5), (1, 4), (1, 5), (2, 4)]]
-    L = [[(0, 4), (1, 4), (2, 4), (2, 5)], [(0, 5), (1, 3), (1, 4), (1, 5)], [(0, 4), (0, 5), (1, 5), (2, 5)], [(0, 4), (0, 5), (0, 6), (1, 4)]]
-    J = [[(0, 5), (1, 5), (2, 4), (2, 5)], [(0, 3), (0, 4), (0, 5), (1, 5)], [(0, 4), (0, 5), (1, 4), (2, 4)], [(0, 4), (1, 4), (1, 5), (1, 6)]]
-    T = [[(0, 4), (1, 4), (1, 5), (2, 4)], [(0, 4), (1, 3), (1, 4), (1, 5)], [(0, 5), (1, 4), (1, 5), (2, 5)], [(0, 4), (0, 5), (0, 6), (1, 5)]]
+    L = [[(0, 4), (1, 4), (2, 4), (2, 5)], [(0, 5), (1, 3), (1, 4), (1, 5)], [(0, 4), (0, 5), (1, 5), (2, 5)],
+         [(0, 4), (0, 5), (0, 6), (1, 4)]]
+    J = [[(0, 5), (1, 5), (2, 4), (2, 5)], [(0, 3), (0, 4), (0, 5), (1, 5)], [(0, 4), (0, 5), (1, 4), (2, 4)],
+         [(0, 4), (1, 4), (1, 5), (1, 6)]]
+    T = [[(0, 4), (1, 4), (1, 5), (2, 4)], [(0, 4), (1, 3), (1, 4), (1, 5)], [(0, 5), (1, 4), (1, 5), (2, 5)],
+         [(0, 4), (0, 5), (0, 6), (1, 5)]]
     figures = {'I': I, 'S': S, 'Z': Z, 'L': L, 'J': J, 'T': T, 'O': O}
 
     dimensions = tuple(int(i) for i in input().split())
@@ -127,15 +106,17 @@ def main():
         comm = input()
         if comm == 'piece':
             if figure:
-                occupied.extend(get_indexes(figure[0]))
-            figure = [from_ind(i, dimensions) for i in figures[input()]]
-        if comm == 'rotate' and not on_floor(figure[0]) and not on_border(occupied, figure[0]):
+                occupied.extend(figure[0])
+            figure = [i for i in figures[input()]]
+        if comm == 'rotate' and not on_floor(figure[0], dimensions) and not on_border(occupied, figure[0]):
             figure = rotate(figure)
-        if comm in 'right_left' and not on_floor(figure[0]) and not on_border(occupied, figure[0]):
-            figure = [move(i, direction=comm) for i in figure]
+        if comm in 'right_left' and not on_floor(figure[0], dimensions) and not on_border(occupied, figure[0]):
+            figure = [move(i, direction=comm, dimensions=dimensions) for i in figure]
         if comm == 'exit':
             break
-        whole = get_indexes(figure[0]) + occupied
+
+        whole = figure[0] + occupied
+
         if comm == 'break':
             for _ in range(dimensions[1]):
                 whole = disappear(whole, dimensions)
@@ -148,9 +129,9 @@ def main():
             break
 
         if not on_border(occupied, figure[0]) and figure[0]:
-            figure = [move(i) for i in figure]
+            figure = [move(i, dimensions=dimensions) for i in figure]
 
-        if finish(get_indexes(figure[0])):
+        if finish(figure[0]):
             game_over = 'Game Over!'
 
 
